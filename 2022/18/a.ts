1f05b81ab8af86e983ac4672a15d302f9b11acc4
Jim Hoskins <jim@jimhoskins.com>
2022-12-18 12:08:15 -0800

    Refactor 2022/18/a.ts

--- 2022/18/a.ts
+++ 2022/18/a.ts
@@ -1,10 +1,8 @@
 export const __ = "";
 import * as fs from "fs";
 
-const file = fs.readFileSync("./input.txt");
-const input = file.toString().trim();
-
-const test_input = `
+const puzzle_input = fs.readFileSync("./input.txt").toString().trim();
+const sample_input = `
 2,2,2
 1,2,2
 3,2,2
@@ -26,6 +24,126 @@ type Cube = {
   y: number;
   z: number;
 };
+type Coord = [number, number, number];
+
+class Simulation {
+  lava = new Map<string, Cube>();
+  outside = new Map<string, Cube>();
+  cubes: Cube[] = [];
+
+  bounds = {
+    x: { min: Infinity, max: -Infinity },
+    y: { min: Infinity, max: -Infinity },
+    z: { min: Infinity, max: -Infinity },
+  };
+
+  constructor(input: string, public readonly name: string) {
+    this.parseInput(input);
+  }
+
+  parseInput(input: string) {
+    const { lava } = this;
+    const lines = input.split("\n");
+    this.cubes = lines.map((line, y) => {
+      const nums = line.split(",").map(Number);
+      const cube = {
+        key: line,
+        x: nums[0],
+        y: nums[1],
+        z: nums[2],
+      };
+      lava.set(cube.key, cube);
+      this.updateBounds(cube);
+      return cube;
+    });
+  }
+
+  updateBounds(cube: Cube) {
+    const {
+      bounds: { x, y, z },
+    } = this;
+    x.min = Math.min(x.min, cube.x);
+    x.max = Math.max(x.max, cube.x);
+    y.min = Math.min(y.min, cube.y);
+    y.max = Math.max(y.max, cube.y);
+    z.min = Math.min(z.min, cube.z);
+    z.max = Math.max(z.max, cube.z);
+  }
+  outOfBounds(key: string) {
+    const { bounds } = this;
+    const [x, y, z] = key.split(",").map(Number);
+    return (
+      x < bounds.x.min ||
+      x > bounds.x.max ||
+      y < bounds.y.min ||
+      y > bounds.y.max ||
+      z < bounds.z.min ||
+      z > bounds.z.max
+    );
+  }
+
+  fillOutside() {
+    const { outside, bounds, lava } = this;
+    const queue = [newCube(bounds.x.min, bounds.y.min, bounds.z.min)];
+
+    while (queue.length) {
+      const cube = queue.shift()!;
+
+      if (outside.has(cube.key)) continue;
+      if (lava.has(cube.key)) continue;
+      if (this.outOfBounds(cube.key)) continue;
+
+      outside.set(cube.key, cube);
+      neighborCoords(cube).forEach((coords) => {
+        queue.push(newCube(...coords));
+      });
+    }
+  }
+
+  // Part 1, find all lava faces thaty are not touching another lava cube
+  findSurfaceArea() {
+    const { cubes, lava, name } = this;
+
+    const surface = cubes.reduce(
+      (acc, cube) =>
+        neighborKeys(cube).reduce(
+          (acc, key) => (!lava.has(key) ? acc + 1 : acc),
+          acc
+        ),
+      0
+    );
+
+    console.log(name, "Part 1:", surface);
+  }
+
+  // Part 2, fill the outside with "outside" cubes, then find all lava cubes
+  // that are touching an outside cube
+  findExternalSurfaceArea() {
+    const { cubes, name, outside, bounds } = this;
+    // Push bounds outward one to fill with "outside" cubes
+    bounds.x.min--;
+    bounds.x.max++;
+    bounds.y.min--;
+    bounds.y.max++;
+    bounds.z.min--;
+    bounds.z.max++;
+
+    // Create outside cubes
+    this.fillOutside();
+
+    // Loop through all the lava, for each outside cube it touches,
+    // it must be exposed to the outside and increase its outside surface area
+    const outerSurface = cubes.reduce(
+      (acc, cube) =>
+        neighborKeys(cube).reduce(
+          (acc, key) => (outside.has(key) ? acc + 1 : acc),
+          acc
+        ),
+      0
+    );
+    console.log(name, "Part 2:", outerSurface);
+  }
+}
 
 function newCube(x: number, y: number, z: number): Cube {
   return {
@@ -35,73 +153,6 @@ function newCube(x: number, y: number, z: number): Cube {
     z,
   };
 }
-
-const lava = new Map<string, Cube>();
-const outside = new Map<string, Cube>();
-
-const bounds = {
-  x: { min: Infinity, max: -Infinity },
-  y: { min: Infinity, max: -Infinity },
-  z: { min: Infinity, max: -Infinity },
-};
-
-function updateBounds(cube: Cube) {
-  bounds.x.min = Math.min(bounds.x.min, cube.x);
-  bounds.x.max = Math.max(bounds.x.max, cube.x);
-  bounds.y.min = Math.min(bounds.y.min, cube.y);
-  bounds.y.max = Math.max(bounds.y.max, cube.y);
-  bounds.z.min = Math.min(bounds.z.min, cube.z);
-  bounds.z.max = Math.max(bounds.z.max, cube.z);
-}
-
-function outOfBounds(key: string) {
-  const [x, y, z] = key.split(",").map(Number);
-  return (
-    x < bounds.x.min ||
-    x > bounds.x.max ||
-    y < bounds.y.min ||
-    y > bounds.y.max ||
-    z < bounds.z.min ||
-    z > bounds.z.max
-  );
-}
-
-// Use BFS to find all the outside cubes starting at a known outside cube
-// Fill `outside` with all the outside cubes
-function fillOutside() {
-  const queue = [newCube(bounds.x.min, bounds.y.min, bounds.z.min)];
-
-  while (queue.length) {
-    const cube = queue.shift()!;
-
-    if (outside.has(cube.key)) continue;
-    if (lava.has(cube.key)) continue;
-    if (outOfBounds(cube.key)) continue;
-
-    outside.set(cube.key, cube);
-    neighborCoords(cube).forEach((coords) => {
-      queue.push(newCube(...coords));
-    });
-  }
-}
-
-function parseInput(input: string) {
-  const lines = input.split("\n");
-  return lines.map((line, y) => {
-    const nums = line.split(",").map(Number);
-    const cube = {
-      key: line,
-      x: nums[0],
-      y: nums[1],
-      z: nums[2],
-    };
-    lava.set(cube.key, cube);
-    updateBounds(cube);
-    return cube;
-  });
-}
-
-type Coord = [number, number, number];
 
 function neighborCoords(cube: Cube): Coord[] {
   const { x, y, z } = cube;
@@ -119,45 +170,15 @@ function neighborKeys(cube: Cube): string[] {
   return neighborCoords(cube).map((coords) => coords.join(","));
 }
 
-function run(input: string) {
-  const cubes = parseInput(input);
+function run(input: string, name: string) {
+  const sim = new Simulation(input, name);
 
   // Part 1
-  const surface = cubes.reduce(
-    (acc, cube) =>
-      neighborKeys(cube).reduce(
-        (acc, key) => (!lava.has(key) ? acc + 1 : acc),
-        acc
-      ),
-    0
-  );
-
-  console.log("Part 1:", surface);
+  sim.findSurfaceArea();
 
   // Part 2
-  // Push bounds outward one to fill with "outside" cubes
-  bounds.x.min--;
-  bounds.x.max++;
-  bounds.y.min--;
-  bounds.y.max++;
-  bounds.z.min--;
-  bounds.z.max++;
-
-  // Create outside cubes
-  fillOutside();
-
-  // Loop through all the lava, for each outside cube it touches,
-  // it must be exposed to the outside and increase its outside surface area
-  const outerSurface = cubes.reduce(
-    (acc, cube) =>
-      neighborKeys(cube).reduce(
-        (acc, key) => (outside.has(key) ? acc + 1 : acc),
-        acc
-      ),
-    0
-  );
-  console.log("Part 2:", outerSurface);
+  sim.findExternalSurfaceArea();
 }
 
-//run(test_input);
-run(input);
+run(sample_input, "Sample");
+run(puzzle_input, "Puzzle");
